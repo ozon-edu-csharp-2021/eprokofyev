ffcd77fe1c56ac1560d35db69b9725c43d01a157
Prokofyev-Evg <Prokofyev.Evg@yandex.ru>
2021-10-19 14:20:34 +0300

    Project created, docker compose configured

---
+++ src/OzonEdu.MerchandiseService/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OzonEdu.MerchandiseService;
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
