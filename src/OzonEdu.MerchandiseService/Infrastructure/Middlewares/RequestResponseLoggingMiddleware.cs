c060901ec928842db3885439c55d3c0a78df88fc
evgeniy_prokofyev <prokofyev.evg@yandex.ru>
2021-11-01 00:10:26 +0300

    Update RequestResponseLoggingMiddleware.cs

--- src/OzonEdu.MerchandiseService/Infrastructure/Middlewares/RequestResponseLoggingMiddleware.cs
+++ src/OzonEdu.MerchandiseService/Infrastructure/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -19,20 +20,71 @@ namespace OzonEdu.MerchandiseService.Infrastructure.Middlewares
 
         public async Task InvokeAsync(HttpContext context)
         {
-            LogRequest(context);
+            await LogRequest(context);
             await _next(context);
-            LogResponse(context);
+            await LogResponse(context);
         }
 
-        private void LogRequest(HttpContext context)
+        private async Task LogRequest(HttpContext context)
         {
-            _logger.LogInformation(context.Request.Headers.ToString());
-            _logger.LogInformation(context.Request.RouteValues.ToString());
+            _logger.LogInformation("Request logged");
+            _logger.LogInformation("Request Headers:");
+            foreach (var item in context.Request.Headers)
+            {
+                _logger.LogInformation($"{item.Key} - {item.Value}");
+            }
+
+            _logger.LogInformation("Request RouteValues:");
+            foreach (var item in context.Request.RouteValues)
+            {
+                _logger.LogInformation($"{item.Key} - {item.Value}");
+            }
+
+            try
+            {
+                if (context.Request.ContentLength > 0)
+                {
+                    context.Request.EnableBuffering();
+
+                    var buffer = new byte[context.Request.ContentLength.Value];
+                    await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
+                    var bodyAsText = Encoding.UTF8.GetString(buffer);
+                    _logger.LogInformation(bodyAsText);
+
+                    context.Request.Body.Position = 0;
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Could not log request body");
+            }
         }
 
-        private void LogResponse(HttpContext context)
+        private async Task LogResponse(HttpContext context)
         {
-            _logger.LogInformation(context.Response.Headers.ToString());
+            _logger.LogInformation("Response logged");
+            _logger.LogInformation("Response Headers:");
+            foreach (var item in context.Response.Headers)
+            {
+                _logger.LogInformation($"{item.Key} - {item.Value}");
+            }
+
+            try
+            {
+                if (context.Response.ContentLength > 0)
+                {
+                    var buffer = new byte[context.Response.ContentLength.Value];
+                    await context.Response.Body.ReadAsync(buffer, 0, buffer.Length);
+                    var bodyAsText = Encoding.UTF8.GetString(buffer);
+                    _logger.LogInformation(bodyAsText);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Could not log request body");
+            }
+
+            context.Response.Body = Stream.Null;
         }
     }
 }
